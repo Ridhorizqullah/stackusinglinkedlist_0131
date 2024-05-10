eb7187c4fbbdb6d4ad89c88f0f169ac182580d80
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-10 10:34:36 +0700

    case 1

--- stackusinglinkedlist_0131/stackusinglinkedlist_0131.cpp
+++ stackusinglinkedlist_0131/stackusinglinkedlist_0131.cpp
@@ -79,20 +79,24 @@ int main() {
 		cin >> choice;
 
 		switch (choice) {
-		
+		case 1:
+			cout << "Enter the value to push: ";
+			cin >> value;
+			stack.push(value);
+			break;
 
-}
-
-
-
-
-	
-
-
-
-
-	
+		}
 
 
 
 
+
+
+
+
+
+
+
+
+
+	}
